d6d28c60624034340f5aead104f963609548c878
Luk'ash <143443457+lukaszdylka@users.noreply.github.com>
2026-09-11 10:32:29 +0200

    Podbij wersję buildu po zmianach strony głównej

--- build-version.js
+++ build-version.js
@@ -1,5 +1,5 @@
 /* Szpilplac shared build version */
 (function(root){
   "use strict";
-  root.SZP_BUILD_ID="2026.09.10.1";
+  root.SZP_BUILD_ID="2026.09.11.1";
 })(typeof self!=="undefined"?self:window);
