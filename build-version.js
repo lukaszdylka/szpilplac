25b41ea14fd4715ad428f63a22a2460a452cc8b8
Luk'ash <143443457+lukaszdylka@users.noreply.github.com>
2026-09-22 09:47:17 +0200

    Build 2026.09.22.3

--- build-version.js
+++ build-version.js
@@ -1,5 +1,5 @@
 /* Szpilplac shared build version · 2026.09.22.2 */
 (function(root){
   "use strict";
-  root.SZP_BUILD_ID="2026.09.22.2";
+  root.SZP_BUILD_ID="2026.09.22.3";
 })(typeof self!=="undefined"?self:window);
